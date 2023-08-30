b27848471e224f844e3a0291580255dbbac8519d
User1 <User@DESKTOP-GT59C8J>
2023-08-30 11:43:35 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using MyShop.Data;
+//using MyShopConsole.Services;
+using MyShop.Data.Entities;
+using MyShopConsole.Services;
+
+namespace MyShopConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ShopDataContext shop = new();
+        Product product = new Product()
+        {
+            Name = "Smartphone",
+            Discount = 0,
+            CategoryId = 1,
+            Price = 5000,
+            VendorId=1,
+
+        };
+        shop.Products.Add(product); 
+        shop.SaveChanges();
+        Product product3 = new Product()
+        {
+            Name = "Iphone",
+            Discount = 10,
+            CategoryId = 1,
+            Price = 15000,
+            VendorId = 1,
+
+        };
+       
+
+
+
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Data;
+using MyShop.Data.Entities;
+
+namespace MyShopConsole.Services
+{
+public class ProductService
+    {
+        private ShopDataContext _context;
+
+        public ProductService()
+        {
+            _context = new ShopDataContext();
+        }
+
+        public void Add(Product product,bool isSaveChange=true) 
+        {
+            _context.Products.Add(product);
+            if (isSaveChange)
+            {
+                _context.SaveChanges();
+            }
+        }
+        public Product GetById(int Id)
+        {
+            return _context.Products.Where( p=> p.Id == Id).FirstOrDefault();
+          
+
+        }
+        
+    }
+}
